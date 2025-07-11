636bfa1b2e7d71e27091c49a518b0fa69a98bc10
yy <517449331@qq.com>
2025-07-12 02:20:35 +0900

    test

--- functions/api/submit.ts
+++ functions/api/submit.ts
@@ -90,7 +90,7 @@ export async function onRequestPost({ request, env }) {
       .replace(/-----BEGIN PRIVATE KEY-----/, "")
       .replace(/-----END PRIVATE KEY-----/, "")
       .replace(/\n/g, "")
-    const binary = atob(b64)
+    const binary = b64
     const bytes = new Uint8Array(binary.length)
     for (let i = 0; i < binary.length; i++) {
       bytes[i] = binary.charCodeAt(i)
